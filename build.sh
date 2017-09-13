8b134c977924ab326f040dda2834f7fba8d744a4
hanif-akarin <hanif.ariffin.4326@gmail.com>
2017-09-13 12:33:31 -0400

    updateed build script to accomodate installing SFML

--- build.sh
+++ build.sh
@@ -7,7 +7,15 @@ if [ ! -d "bin" ]; then
 	mkdir bin
 fi
 rm -rf bin/*
-
+if ! dpkg -s libsfml-dev  > /dev/null; then
+	echo -e "SFML not found! Install? (y/n) \c"
+	read
+	if "$REPLY" = "y"; then
+		sudo apt-get install libsfml-dev
+	fi
+else
+	echo "SFML is installed"
+fi
 g++ -c main.cpp -o obj/main.o
 g++ -c engine.cpp -o obj/engine.o
 
